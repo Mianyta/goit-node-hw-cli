392c3876f2a326c5b268ba6e75bd27771a77e501
Mianyta <suta180987@gmail.com>
2024-01-14 19:46:21 +0200

    fix

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-const { listContacts, getContactById, removeContact, addContact } = require('./contacts');
+import * as contactsService from './contacts.js';
+import { program } from 'commander';
 
-(async () => {
-  // Примеры использования функций
-  console.table(await listContacts());
-  console.log(await getContactById('someId'));
-  console.log(await removeContact('someId'));
-  console.log(await addContact('John Doe', 'john@example.com', '123-456-789'));
-})();
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+          const allContacts = await contactsService.listContacts();
+          console.table(allContacts);
+      break;
+    case 'get':
+          const getContactById = await contactsService.getContactById(id);
+          console.log(getContactById);
+      break;
+    case 'add':
+          const addNewContact = await contactsService.addContact(
+            name,
+            email,
+            phone
+          );
+          console.log(addNewContact);
+      break;
+    case 'remove':
+          const removeContact = await contactsService.removeContact(id);
+          console.log(removeContact);
+          break;
+      
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+};
+
+program
+  .option('--action <type>')
+  .option('--id <type>')
+  .option('--name <type>')
+  .option('--email <type>')
+  .option('--phone <type>');
+  
+program.parse();
+
+const argv = program.opts();
+invokeAction(argv);
